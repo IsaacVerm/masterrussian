0e18e43e424d0933de72fa4b8461921ea71f978f
Isaac Verminck <isaacverm@gmail.com>
2020-02-14 16:07:38 +0100

    add cum_word_length function

--- R/word-length.R
+++ R/word-length.R
@@ -3,6 +3,13 @@ add_russian_word_length <- function(words) {
     dplyr::mutate(russian_word_length = stringr::str_length(russian) - 1)
 }
 
+cum_word_length <- function(words, word_length) {
+  words %>%
+    dplyr::filter(russian_word_length <= word_length) %>%
+    dplyr::pull(russian_word_length) %>%
+    sum()
+}
+
 histogram_word_length <- function(words) {
   ggplot2::ggplot(data = words,
                   ggplot2::aes(x = russian_word_length)) +
