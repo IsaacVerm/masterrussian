84db163480f5f90253604530da5a6407fc19b3f1
Isaac Verminck <isaacverm@gmail.com>
2020-02-14 15:22:59 +0100

    add histogram_word_length function

--- R/word-length.R
+++ R/word-length.R
@@ -2,3 +2,9 @@ add_russian_word_length <- function(words) {
   words %>%
     dplyr::mutate(russian_word_length = stringr::str_length(russian) - 1)
 }
+
+histogram_word_length <- function(words) {
+  ggplot2::ggplot(data = words,
+                  ggplot2::aes(x = russian_word_length)) +
+    ggplot2::geom_histogram(binwidth = 1)
+}
